d1c37449ca501d58b862d44bb43a5871746ec892
Ryan Price <rsprice90@gmail.com>
2015-11-17 14:39:38 -0800

    Basic scaffolding.

---
+++ public/src/app.js
@@ -0,0 +1,17 @@
+// instantiate an angular app
+var app = angular.module('app', []);
+  // declare one controller for the app
+app.controller('appCtrl', function($scope, appFactory) {
+  // * scope will have the query string as a variable
+  // * show meta data as an object (reponse from AJAX call?)
+  // * d3 object / data set (when data is changed page is update)
+  // * search function
+    // - make call to AJAX factory
+
+});
+
+// helper function / factory for AJAX
+app.factory('appFactory', function($http) {
+  // needs $http for AJAX functionality
+  
+});
