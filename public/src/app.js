00327749623a35e3d7514b7c93c406e71626f075
Eric Hollenberry <erichollenberry@gmail.com>
2015-11-18 11:39:24 -0800

    (fix) - Adjusting for Merge Conflicts

--- public/src/app.js
+++ public/src/app.js
@@ -4,13 +4,10 @@ var app = angular.module('app', []);
 app.controller('appCtrl', function($scope, appFactory) {
   // * scope will have the query string as a variable
   // * show meta data as an object (reponse from AJAX call?)
-<<<<<<< 36a7477548c90d84bd96157fb71c23023f029f56
   $scope.results = [];
-=======
->>>>>>> (feat) - Added d3 Graph(Axes/Data Points) and Styling
+
   // * d3 object / data set (when data is changed page is update)
   // * search function
-<<<<<<< 36a7477548c90d84bd96157fb71c23023f029f56
   $scope.submit = function() {
     // - make call to AJAX factory
     var season = 1;
@@ -34,14 +31,4 @@ app.controller('appCtrl', function($scope, appFactory) {
     };
     getAllSeasons(season);
   };
-=======
-    // - make call to AJAX factory
-  $scope.test = 'testing';
-});
-
-// helper function / factory for AJAX
-app.factory('appFactory', function($http) {
-  // needs $http for AJAX functionality
-  return {};
->>>>>>> (feat) - Added d3 Graph(Axes/Data Points) and Styling
 });
